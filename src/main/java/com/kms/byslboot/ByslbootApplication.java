0a4a10499d65b1d9eb239d0384689a1013429c65
aqp1234 <63453828+aqp1234@users.noreply.github.com>
2022-04-11 16:11:33 +0900

    Initial Commit

--- src/main/java/com/kms/byslboot/ByslbootApplication.java
+++ src/main/java/com/kms/byslboot/ByslbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ByslbootApplication {
+public class ByslBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ByslbootApplication.class, args);
+		SpringApplication.run(ByslBootApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/ByslBootApplicationTests.java
+++ src/test/java/com/example/demo/ByslBootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kms.byslboot;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ByslbootApplicationTests {
+class ByslBootApplicationTests {
 
 	@Test
 	void contextLoads() {
