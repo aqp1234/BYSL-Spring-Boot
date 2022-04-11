aa0fab6e1d6b44f3a76197f27c9500ad2e36359d
aqp1234 <63453828+aqp1234@users.noreply.github.com>
2022-04-11 15:49:27 +0900

    Initial commit

---
+++ src/main/java/com/kms/byslboot/ByslbootApplication.java
@@ -0,0 +1,13 @@
+package com.kms.byslboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByslbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByslbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kms/byslboot/ByslbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kms.byslboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByslbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
