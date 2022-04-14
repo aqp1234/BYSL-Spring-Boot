c3741de8fb355a878ba614adf55f0e54a73ab995
aqp1234 <63453828+aqp1234@users.noreply.github.com>
2022-04-14 21:51:12 +0900

    HttpStatus Created status code 삭제 (따로 build)

--- src/main/java/com/kms/byslboot/common/ResponseEntityHttpStatus.java
+++ src/main/java/com/kms/byslboot/common/ResponseEntityHttpStatus.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 
 public class ResponseEntityHttpStatus {
 	public static final ResponseEntity<HttpStatus> RESPONSE_OK = ResponseEntity.status(HttpStatus.OK).build();
-	public static final ResponseEntity<HttpStatus> RESPONSE_CREATED = ResponseEntity.status(HttpStatus.CREATED).build();
 	public static final ResponseEntity<HttpStatus> RESPONSE_BAD_REQUEST = ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	public static final ResponseEntity<HttpStatus> RESPONSE_UNAUTHORIZED = ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 	public static final ResponseEntity<HttpStatus> RESPONSE_FORBIDDEN = ResponseEntity.status(HttpStatus.FORBIDDEN).build();
